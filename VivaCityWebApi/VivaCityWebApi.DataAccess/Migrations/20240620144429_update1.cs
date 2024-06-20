7bb188f71abe6e9fb49b40137913a357598badc9
sedar <desiradjysedar@gmail.com>
2024-06-20 21:33:53 +0200

    add migration

---
+++ VivaCityWebApi/VivaCityWebApi.DataAccess/Migrations/20240620144429_update1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VivaCityWebApi.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Batiments_Couts_Id",
+                table: "Batiments");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CoutId",
+                table: "Batiments",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Batiments_CoutId",
+                table: "Batiments",
+                column: "CoutId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Batiments_Couts_CoutId",
+                table: "Batiments",
+                column: "CoutId",
+                principalTable: "Couts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Batiments_Couts_CoutId",
+                table: "Batiments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Batiments_CoutId",
+                table: "Batiments");
+
+            migrationBuilder.DropColumn(
+                name: "CoutId",
+                table: "Batiments");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Batiments_Couts_Id",
+                table: "Batiments",
+                column: "Id",
+                principalTable: "Couts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ VivaCityWebApi/VivaCityWebApi.DataAccess/Migrations/20240620154502_update2.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VivaCityWebApi.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class update2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ressources_RessourceItem_Id",
+                table: "Ressources");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RessourceItemId",
+                table: "Ressources",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ressources_RessourceItemId",
+                table: "Ressources",
+                column: "RessourceItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ressources_RessourceItem_RessourceItemId",
+                table: "Ressources",
+                column: "RessourceItemId",
+                principalTable: "RessourceItem",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ressources_RessourceItem_RessourceItemId",
+                table: "Ressources");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ressources_RessourceItemId",
+                table: "Ressources");
+
+            migrationBuilder.DropColumn(
+                name: "RessourceItemId",
+                table: "Ressources");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ressources_RessourceItem_Id",
+                table: "Ressources",
+                column: "Id",
+                principalTable: "RessourceItem",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
